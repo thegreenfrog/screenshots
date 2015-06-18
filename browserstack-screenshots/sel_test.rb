9489c24e3a17908fc4e6338548129ae67ab66d44
Andrew Borstein <andrewborstein@Andrews-MacBook-Pro-2.local>
2015-06-18 17:30:16 -0400

    Newest cbt-test.rb, screenshots folder, snake_case file names, example screenshots

--- browserstack-screenshots/sel_test.rb
+++ browserstack-screenshots/sel_test.rb
@@ -11,9 +11,9 @@ caps["os_api_name"] = "Mac10.9"
 caps["screen_resolution"] = "1024x768"
 caps["record_video"] = "true"
 caps["record_network"] = "true"
-caps["record_snapshot"] = "false" 
+caps["record_snapshot"] = "false"
 
-driver = Selenium::WebDriver.for(:remote, 
+driver = Selenium::WebDriver.for(:remote,
 :url => "http://aborstein%40wespire.com:u56426565207b7b9@hub.crossbrowsertesting.com:80/wd/hub",
 :desired_capabilities => caps)
 
